e55d364ab6ac478f143f12c3e8f5eb54a108c1e7
FrankHamp <valdemarengelund@gmail.com>
2026-02-09 14:44:29 +0100

    1

--- src/Diceman/DicemanTest.java
+++ src/Diceman/DicemanTest.java
@@ -1,4 +1,33 @@
 package Diceman;
 
-public class DicemanTest {
+    public class DicemanTest {
+        public static void main(String[] args) {
+            Diceman myDice = new Diceman();
+            int DiceCount = 0;
+            int totalRolls = 0;
+
+
+            while (DiceCount < 6) {
+                myDice.Roll();
+                totalRolls++;
+                System.out.println("Roll " + totalRolls + ": " + myDice);
+
+
+                switch (myDice.getDice()) {
+                    case "":
+                        DiceCount++;
+                        break;
+                    case "":
+                        DiceCount++;
+                        break;
+                    default:
+                        System.out.println("Unknown result!");
+                }
+            }
+
+
+            System.out.println("\nFinished!");
+            System.out.println("Total Rolls: " + totalRolls);
+            System.out.println("Dice Count: " + DiceCount);
+        }
 }
